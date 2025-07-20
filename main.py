a5c498139e0624fcd8dc34096ce6242e5dc055d0
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-20 19:38:46 +0530

    new game method

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import pygame as pg
 import sys
 from settings import *
 
-def __init__():
-    pg.init()
-    self.screen = pg.display.set_mode(RES)
-    self.clock = pg.time.Clock()
-
+class Game:
+    def __init__():
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
     
+    def new_game():
+        pass
+    
+     

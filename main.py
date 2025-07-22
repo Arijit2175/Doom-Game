63a3a7fb00e0f9d20591a5d59c08af03f76236b5
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-22 23:13:08 +0530

    added the objects

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import sys
 from settings import *
 
 class Game:
-    def __init__():
+    def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
     
-    def new_game():
+    def new_game(self):
         pass
     
-    def update():
+    def update(self):
         pg.display.flip()
         self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')

699bffd187d4c2f5ce4f69fcb6059d9877b7cc55
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-23 20:17:36 +0530

    instance function

--- player.py
+++ player.py
@@ -2,3 +2,9 @@ import pygame as pg
 from settings import *
 import math
 
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+

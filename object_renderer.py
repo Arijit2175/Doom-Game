73c41159e76f0e04292853ae78961aa479b84d5a
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-24 21:09:02 +0530

    find texture and return a scaled image

--- object_renderer.py
+++ object_renderer.py
@@ -6,4 +6,7 @@ class ObjectRenderer:
         self.game = game
         self.screen = game.screen
 
-        
+    @staticmethod
+    def get_texture(path, res = (TEXTURE_SIZE, TEXTURE_SIZE)):
+        texture = pg.image.load(path).convert_alpha()
+        return pg.transform.scale(texture, res)

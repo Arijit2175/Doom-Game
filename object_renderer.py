e3776e571cb44fe35d1252498278e893bc923244
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-24 21:11:10 +0530

    calling texture loading method

--- object_renderer.py
+++ object_renderer.py
@@ -5,6 +5,7 @@ class ObjectRenderer:
     def __init__(self, game):
         self.game = game
         self.screen = game.screen
+        self.wall_textures = self.load_wall_textures()
 
     @staticmethod
     def get_texture(path, res = (TEXTURE_SIZE, TEXTURE_SIZE)):

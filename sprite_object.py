1a93e837dab51e1bdb7ae4a3c27fb9de7dc045f4
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-27 17:10:39 +0530

    changed a variable

--- sprite_object.py
+++ sprite_object.py
@@ -7,9 +7,9 @@ class SpriteObject:
         self.player = game.player
         self.x, self.y = pos
         self.image = pg.image.load(path).convert_alpha()
-        self.WIDTH = self.image.get_width()
+        self.IMAGE_WIDTH = self.image.get_width()
         self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
-        self.IMAGE_RATIO = self.IMAGE_HALF_WIDTH / self.image.get_height() 
+        self.IMAGE_RATIO = self.IMAGE_WIDTH / self.image.get_height() 
         self.dx, self.dy, self.theta, self.screen_x, self.dist, self.norm_dist = 0, 0, 0, 0, 1, 1
         self.sprite_half_width = 0
 

9c2d52f36ac11de1336b9f1b0a7a7b3754bc7707
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-27 20:09:25 +0530

    compiled

--- sprite_object.py
+++ sprite_object.py
@@ -76,8 +76,8 @@ class AnimatedSprite(SpriteObject):
         self.animation_trigger = False
         time_now = pg.time.get_ticks()
         if time_now - self.animation_time_prev > self.animation_time:
-            self.animation_trigger = True
             self.animation_time_prev = time_now
+            self.animation_trigger = True
 
     def get_images(self, path):
         images = deque()

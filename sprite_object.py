c2d9c7e4fdd763b55c5593f37bc08c18c7fd5cfe
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-27 16:54:48 +0530

    to find the theta angle

--- sprite_object.py
+++ sprite_object.py
@@ -11,7 +11,12 @@ class SpriteObject:
         self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
 
     def get_sprite(self):
-        pass
+        dx = self.x - self.player.x
+        dy = self.y - self.player.y
+        self.dx, self.dy = dx, dy
+        self.theta = math.atan2(dy, dx)
+
+        
 
     def update(self):
         self.get_sprite()

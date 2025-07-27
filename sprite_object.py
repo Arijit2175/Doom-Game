1652b79ebbe187421237d05fbc0efb92b40c7995
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-27 16:52:14 +0530

    for instances

--- sprite_object.py
+++ sprite_object.py
@@ -1,3 +1,13 @@
 import pygame as pg
 from settings import *
 
+class SpriteObject:
+    def __init__(self, game, path='assets/sprites/static_sprites/candlebra.png', pos=(10.5, 3.5)):
+        self.game = game
+        self.player = game.player
+        self.x, self.y = pos
+        self.image = pg.image.Load(path).convert_alpha()
+        self.WIDTH = self.image.get_width()
+        self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
+
+    

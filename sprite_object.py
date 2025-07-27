8dbe34e2b23d23f7146016703e0609b085ebb124
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-27 16:52:55 +0530

    for updating the game

--- sprite_object.py
+++ sprite_object.py
@@ -10,4 +10,10 @@ class SpriteObject:
         self.WIDTH = self.image.get_width()
         self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
 
+    def get_sprite(self):
+        pass
+
+    def update(self):
+        self.get_sprite()
+
     

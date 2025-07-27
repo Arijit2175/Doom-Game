c690fc7db6e183fdd48b4c98f74fe42af36e7e34
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-27 20:16:09 +0530

    initializing variables

--- object_handler.py
+++ object_handler.py
@@ -1,2 +1,10 @@
 from sprite_object import *
 
+class ObjectHandler:
+    def __init__(self, game):
+        self.game = game
+        self.sprite_list = []
+        self.static_sprite_path = 'assets/sprites/static_sprites/'
+        self.anim_sprite_path = 'assets/sprites/animated_sprites/'
+
+    

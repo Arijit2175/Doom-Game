01217de7768573e0b72e14ea90ebbd55edb95ef5
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-27 17:05:14 +0530

    for projection of sprites on the map

--- sprite_object.py
+++ sprite_object.py
@@ -9,6 +9,18 @@ class SpriteObject:
         self.image = pg.image.Load(path).convert_alpha()
         self.WIDTH = self.image.get_width()
         self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
+        self.IMAGE_RATIO = self.IMAGE_WIDTH / self.image.get_height() 
+
+    def get_sprite_projection(self):
+        proj = SCREEN_DIST / self.norm_dist
+        proj_width, proj_height = proj * self.IMAGE_RATIO, proj
+
+        image = pg.transform.scale(self.image, (proj_width, proj_height))
+
+        self.sprite_half_width = proj_width // 2
+        pos = self.screen_x - self.sprite_half_width, HALF_HEIGHT - proj_height // 2
+
+        self.game.raycasting.objects_to_render.append((self.norm_dist, image, pos))
 
     def get_sprite(self):
         dx = self.x - self.player.x

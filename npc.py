93e23a9145c6160b41ff311eac4f096923a3d9f6
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-29 23:18:44 +0530

    applying ray casting within npc and player

--- npc.py
+++ npc.py
@@ -45,3 +45,71 @@ class NPC(AnimatedSprite):
                 self.animate_pain()
             else:
                 self.animate(self.idle_images)
+
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
+    
+    def ray_cast_npc_player(self):
+        if self.game.player.map_pos == self.map_pos:
+            return True
+        
+        wall_dist_v, wall_dist_h = 0, 0
+        player_dist_v, player_dist_h = 0, 0
+
+        ox, oy = self.game.player.pos
+        x_map, y_map = self.game.player.map_pos
+
+        
+        ray_angle = self.theta
+        sin_a = math.sin(ray_angle)
+        cos_a = math.cos(ray_angle)
+
+        #horizontals
+        y_hort, dy = (y_map + 1, 1) if sin_a > 0 else (y_map - 1e-6, -1)
+        depth_hort = (y_hort - oy) / sin_a
+        x_hort = ox + depth_hort * cos_a
+        delta_depth = dy / sin_a
+        dx = delta_depth * cos_a
+
+        for i in range(MAX_DEPTH):
+            title_hort = int(x_hort), int(y_hort)
+            if title_hort in self.map_pos:
+                player_dist_h = depth_hort
+                break
+            if title_hort in self.game.map.world_map:
+                wall_dist_h = depth_hort
+                break
+            x_hort += dx
+            y_hort += dy
+            depth_hort += delta_depth
+
+        #verticals
+        x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+
+        depth_vert = (x_vert - ox) / cos_a
+        y_vert = oy + depth_vert * sin_a
+
+        delta_depth = dx / cos_a
+        dy = delta_depth * sin_a
+
+        for i in range(MAX_DEPTH):
+            title_vert = int(x_vert), int(y_vert)
+            if title_vert == self.map_pos:
+                player_dist_v = depth_vert
+                break
+            if title_vert in self.game.map.world_map:
+                wall_dist_v = depth_vert
+                break
+            x_vert += dx
+            y_vert += dy
+            depth_vert += delta_depth
+        
+        player_dist = max(player_dist_v, player_dist_h)
+        wall_dist = max(wall_dist_v, wall_dist_h)
+
+        if 0 < player_dist < wall_dist or not wall_dist:
+            return True
+        return False
+    
+    

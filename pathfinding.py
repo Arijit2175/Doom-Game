7c1d8f1c7980ceb941f45ef185ad0b997257b040
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-29 23:59:45 +0530

    to get the graph for the npc to follow

--- pathfinding.py
+++ pathfinding.py
@@ -8,4 +8,8 @@ class PathFinding:
         self.graph = {}
         self.get_graph()
 
-    
+    def get_graph(self):
+        for y, row in enumerate(self.map):
+            for x, col in enumerate(row):
+                if not col:
+                    self.graph[(x, y)] = self.graph.get((x, y), []) + self.get_next_nodes(x, y)

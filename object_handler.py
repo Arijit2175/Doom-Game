68150846f3a3df6ac82d19c12d8263280bd6bfc4
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-30 20:25:21 +0530

    method for victory

--- object_handler.py
+++ object_handler.py
@@ -53,6 +53,13 @@ class ObjectHandler:
                     pos = x, y = randrange(self.game.map.cols), randrange(self.game.map.rows)
                 self.add_npc(npc(self.game, pos=(x + 0.5, y + 0.5)))
 
+    def check_win(self):
+        if not len(self.npc_positions):
+            self.game.object_renderer.win()
+            pg.display.flip()
+            pg.time.delay(1500)
+            self.game.new_game()
+
     def update(self):
         self.npc_positions = {npc.map_pos for npc in self.npc_list if npc.alive}
         [sprite.update() for sprite in self.sprite_list]

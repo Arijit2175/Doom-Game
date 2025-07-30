c3637a6c75e38df46a4b58f1a3bfd26dad166990
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-30 20:22:04 +0530

    added cyber demon properties

--- npc.py
+++ npc.py
@@ -196,3 +196,12 @@ class CacoDemonNPC(NPC):
         self.speed = 0.05
         self.accuracy = 0.35
 
+class CyberDemonNPC(NPC):
+    def __init__(self, game, path='resources/sprites/npc/cyber_demon/0.png', pos=(11.5, 6.0),
+                 scale=1.0, shift=0.04, animation_time=210):
+        super().__init__(game, path, pos, scale, shift, animation_time)
+        self.attack_dist = 6
+        self.health = 350
+        self.attack_damage = 15
+        self.speed = 0.055
+        self.accuracy = 0.25

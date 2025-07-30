139fbe2d859ce82706181edcc1a96c4e16f4e3a1
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-30 20:26:29 +0530

    added victory image

--- object_renderer.py
+++ object_renderer.py
@@ -14,6 +14,7 @@ class ObjectRenderer:
                              for i in range(11)]
         self.digits = dict(zip(map(str, range(11)), self.digit_images))
         self.game_over_image = self.get_texture('assets/textures/game_over.png', RES)
+        self.win_image = self.get_texture('assets/textures/win.png', RES)
 
     def draw(self):
         self.draw_background()

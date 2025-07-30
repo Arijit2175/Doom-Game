da9f4c4b9b6ec738edcbbf29ec04af114c820a26
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-30 20:21:45 +0530

    added cacodemon properties

--- npc.py
+++ npc.py
@@ -185,3 +185,14 @@ class SoldierNPC(NPC):
     def __init__(self, game, path='assets/sprites/npc/Imp/imp_0.png', pos=(10.5, 5.5),
                  scale=0.6, shift=0.38, animation_time=180):
         super().__init__(game, path, pos, scale, shift, animation_time)
+
+class CacoDemonNPC(NPC):
+    def __init__(self, game, path='assets/sprites/npc/caco_demon/0.png', pos=(10.5, 6.5),
+                 scale=0.7, shift=0.27, animation_time=250):
+        super().__init__(game, path, pos, scale, shift, animation_time)
+        self.attack_dist = 1.0
+        self.health = 150
+        self.attack_damage = 25
+        self.speed = 0.05
+        self.accuracy = 0.35
+
